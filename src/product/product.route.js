bc015d5ccf6cefbab5af6e07adae7681e08b9b88
jaisarun53 <jaisarun53@gmail.com>
2024-03-31 20:59:42 +0545

    cart

--- src/product/product.route.js
+++ src/product/product.route.js
@@ -1,7 +1,14 @@
 import { Router } from "express";
-import { isSeller, isUser } from "../middleware/authentication.middleware.js";
+import {
+  isBuyer,
+  isSeller,
+  isUser,
+} from "../middleware/authentication.middleware.js";
 import validateReqBody from "../middleware/validation.middleware.js";
-import { addProductValidationSchema } from "./product.validation.js";
+import {
+  addProductValidationSchema,
+  paginationValidationSchema,
+} from "./product.validation.js";
 import Product from "./product.model.js";
 import validateIdFromReqParams from "../middleware/validate.id.middleware.js";
 const router = Router();
@@ -88,5 +95,119 @@ router.delete(
   }
 );
 // edit product details
+router.put(
+  "/product/edit/:id",
+  isSeller,
+  validateIdFromReqParams,
+  validateReqBody(addProductValidationSchema),
+  async (req, res) => {
+    // extract product id from req.params
+    const productId = req.params.id;
+    // find product by id
+    const product = await Product.findOne({ _id: productId });
+    // if not product, throw error
+    if (!product) {
+      return res.status(404).send({ message: "product doesnot exist" });
+    }
+    // check for product ownership
+    // product's sellerId must be same with loggedInUserId
+    const productOwnerId = product.sellerId;
+    const loggedInUserId = req.loggedInUserId;
+    const isProductOwner = productOwnerId.equals(loggedInUserId);
+    // if not owner of product, throw error
+    if (!isProductOwner) {
+      return res
+        .status(403)
+        .send({ message: "you are not the owner of this product" });
+    }
+    // extract newValues from req.body
+    const newValues = req.body;
+    // edit product
+    await Product.updateOne({ _id: productId }, { $set: { ...newValues } });
+    // send response
+    return res.status(200).send({ message: "Product is Updated Successfully" });
+  }
+);
+// list product by buyer
+router.get(
+  "/product/list/buyer",
+  isBuyer,
+  async (req, res, next) => {
+    //  extract new values from req.body
+    const newValues = req.body;
+    // validate new values using yup schema
+    try {
+      const validatedData = await paginationValidationSchema.validate(
+        newValues
+      );
+      req.body = validatedData;
+    } catch (error) {
+      // if validation fails throw error
+      return res.status(400).send({ message: error.message });
+    }
 
+    // call next function
+    next();
+  },
+  async (req, res) => {
+    const { page, limit } = req.body;
+    const skip = (page - 1) * limit;
+    const products = await Product.aggregate([
+      { $match: {} },
+      { $skip: skip },
+      { $limit: limit },
+      {
+        $project: {
+          name: 1,
+          brand: 1,
+          price: 1,
+          category: 1,
+          freeShipping: 1,
+          image: 1,
+          availableQuantity: 1,
+          description: 1,
+        },
+      },
+    ]);
+    // send response
+    return res.status(200).send({ message: "success", productList: products });
+  }
+);
+
+// list product by seller
+router.get(
+  "/product/list/seller",
+  isSeller,
+  validateReqBody(paginationValidationSchema),
+  async (req, res) => {
+    // extract pagination data from req.body
+    const { page, limit } = req.body;
+
+    // calculate skip
+    const skip = (page - 1) * limit;
+
+    const products = await Product.aggregate([
+      {
+        $match: {
+          sellerId: req.loggedInUserId,
+        },
+      },
+
+      { $skip: skip },
+
+      { $limit: limit },
+
+      {
+        $project: {
+          sellerId: 0,
+          createdAt: 0,
+          updatedAt: 0,
+          __v: 0,
+        },
+      },
+    ]);
+
+    return res.status(200).send({ message: "success", productList: products });
+  }
+);
 export default router;
--- src/product/product.validation.js
+++ src/product/product.validation.js
@@ -39,3 +39,13 @@ export const addProductValidationSchema = Yup.object({
     .min(10, "Description must be at least 10 characters.")
     .max(1000, "Description must be at max 1000 characters."),
 });
+
+export const paginationValidationSchema = Yup.object({
+  page: Yup.number()
+    .min(1, "Page must be at least 1.")
+    .required("Page is required."),
+  limit: Yup.number()
+    .min(1, "Limit must be at least 1.")
+    .required("Limit is required.")
+    .max(100, "Limit must be at max 100."),
+});
